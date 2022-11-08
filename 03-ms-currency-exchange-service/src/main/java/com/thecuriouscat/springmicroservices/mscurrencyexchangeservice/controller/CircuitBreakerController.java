198510d5e6a719502c93dd9393f120d0cccb6a4c
thecuriouscat <theresapan27@gmail.com>
2022-11-08 12:16:46 -0800

    03-ms-CX-service-update-resilience4j

---
+++ 03-ms-currency-exchange-service/src/main/java/com/thecuriouscat/springmicroservices/mscurrencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,45 @@
+package com.thecuriouscat.springmicroservices.mscurrencyexchangeservice.controller;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Description:
+ * @Author: theresa
+ * @Date: 11/8/22
+ */
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse") //if the retry failed 3 times, will return an error message
+    //@CircuitBreaker(name = "default",fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
+    public String sampleApi(){
+
+        logger.info("Sample API call received");
+
+        //ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-negative-url",
+           //     String.class);
+
+        //return forEntity.getBody();
+
+        return "sample-api-rateLimiterTest"; //for ratelimiter test
+    }
+
+    public String hardcodedResponse(Exception ex){
+        return "fall-back response";
+
+    }
+}
