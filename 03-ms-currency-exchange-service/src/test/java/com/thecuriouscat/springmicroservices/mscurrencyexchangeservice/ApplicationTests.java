ccfdfbc0bc5b2765048e19693cdbd77b40a4fac2
thecuriouscat <theresapan27@gmail.com>
2022-11-07 17:34:27 -0800

    03-currency-exchange-service-update

---
+++ 03-ms-currency-exchange-service/src/test/java/com/thecuriouscat/springmicroservices/mscurrencyexchangeservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecuriouscat.springmicroservices.mscurrencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
