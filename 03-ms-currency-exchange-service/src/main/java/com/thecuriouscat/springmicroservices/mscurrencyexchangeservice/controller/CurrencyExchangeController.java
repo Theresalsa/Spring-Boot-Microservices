1ceea8a9b90e37445b3976e6258444a6dcb2bd3b
thecuriouscat <theresapan27@gmail.com>
2022-11-10 09:55:10 -0800

    updates for adding Zipkin

--- 03-ms-currency-exchange-service/src/main/java/com/thecuriouscat/springmicroservices/mscurrencyexchangeservice/controller/CurrencyExchangeController.java
+++ 03-ms-currency-exchange-service/src/main/java/com/thecuriouscat/springmicroservices/mscurrencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.thecuriouscat.springmicroservices.mscurrencyexchangeservice.controll
 
 import com.thecuriouscat.springmicroservices.mscurrencyexchangeservice.dao.CurrencyExchangeRepository;
 import com.thecuriouscat.springmicroservices.mscurrencyexchangeservice.entity.CurrencyExchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,8 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -27,6 +31,8 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to) throws Exception{
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
 
         CurrencyExchange currencyExchange = currencyExchangeRepository.findCurrencyExchangeByFromAndTo(from, to);
         if(currencyExchange == null) throw new RuntimeException("Unable to find data for" + from + "to" + to);
